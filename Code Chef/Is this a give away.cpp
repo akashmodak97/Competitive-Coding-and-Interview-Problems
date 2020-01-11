87bde719600d7cde28e1ee4e111856bc09ec2ff9
Akash Modak <52149475+akashmodak97@users.noreply.github.com>
2020-01-11 13:31:48 +0530

    Is this a give away problem solution added

---
+++ Code Chef/Is this a give away.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int l,r,k;
+	    cin>>l>>r>>k;
+	    if(l!=r)
+	    cout<<k<<endl;
+	    else
+	    cout<<1<<endl;
+	}
+	return 0;
+}
