5fc56d89cde4c0f0baf0792def9b19221f4287bb
Akash Modak <akashmodak97@gmail.com>
2020-07-25 22:48:33 +0530

    Single Number

---
+++ Leet Code/Single Number.cpp
@@ -0,0 +1,30 @@
+/* Leet Code */
+/* Title - Single Number */
+/* Created By - Akash Modak */
+/* Date - 25/7/2020 */
+
+// Given a non-empty array of integers, every element appears twice except for one. Find that single one.
+
+// Note:
+
+// Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
+
+// Example 1:
+
+// Input: [2,2,1]
+// Output: 1
+// Example 2:
+
+// Input: [4,1,2,1,2]
+// Output: 4
+
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int x = 0;
+        for(auto i:nums){
+            x^=i;
+        }
+        return x;
+    }
+};
