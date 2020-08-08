db74e0c7da4657828d51f217a6d0d28cce32a4db
Akash Modak <akashmodak97@gmail.com>
2020-08-08 21:07:54 +0530

    Path Sum III

---
+++ Leet Code/Path Sum III.cpp
@@ -0,0 +1,51 @@
+/* Leet Code */
+/* Title - Path Sum III */
+/* Created By - Akash Modak */
+/* Date - 6/8/2020 */
+
+// You are given a binary tree in which each node contains an integer value.
+
+// Find the number of paths that sum to a given value.
+
+// The path does not need to start or end at the root or a leaf, but it must go downwards (traveling only from parent nodes to child nodes).
+
+// The tree has no more than 1,000 nodes and the values are in the range -1,000,000 to 1,000,000.
+
+// Example:
+
+// root = [10,5,-3,3,2,null,11,3,-2,null,1], sum = 8
+
+//       10
+//      /  \
+//     5   -3
+//    / \    \
+//   3   2   11
+//  / \   \
+// 3  -2   1
+
+// Return 3. The paths that sum to 8 are:
+
+// 1.  5 -> 3
+// 2.  5 -> 2 -> 1
+// 3. -3 -> 11
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int DFS(TreeNode *root,int sum){
+        return root?(root->val==sum) + DFS(root->left,sum-root->val) + DFS(root->right,sum-root->val):0;
+    }
+    int pathSum(TreeNode* root, int sum) {
+        return root? DFS(root,sum) + pathSum(root->left,sum) + pathSum(root->right,sum):0;
+    }
+};
