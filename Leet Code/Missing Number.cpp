565000fb4e0e5ba13de2ee110ff2bde9fad9cd49
Akash Modak <akashmodak97@gmail.com>
2020-08-22 01:27:50 +0530

    Missing Number

---
+++ Leet Code/Missing Number.cpp
@@ -0,0 +1,29 @@
+/* Leet Code */
+/* Title - Missing Number */
+/* Created By - Akash Modak */
+/* Date - 22/8/2020 */
+
+// Given an array containing n distinct numbers taken from 0, 1, 2, ..., n, find the one that is missing from the array.
+
+// Example 1:
+
+// Input: [3,0,1]
+// Output: 2
+// Example 2:
+
+// Input: [9,6,4,2,3,5,7,0,1]
+// Output: 8
+
+
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n = nums.size();
+        int sumn = (n*(n+1))/2;
+        int sum = 0;
+        for(int i=0;i<n;i++){
+            sum+=nums[i];
+        }
+        return sumn-sum;
+    }
+};
