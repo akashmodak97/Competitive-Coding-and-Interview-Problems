c0e749c8ced63890fb837f5431a300f0c22854fc
Akash Modak <akashmodak97@gmail.com>
2020-08-22 01:23:57 +0530

    Update Sort Array By Parity.cpp

--- Leet Code/Sort Array By Parity.cpp
+++ Leet Code/Sort Array By Parity.cpp
@@ -3,17 +3,17 @@
 /* Created By - Akash Modak */
 /* Date - 21/8/2020 */
 
-Given an array A of non-negative integers, return an array consisting of all the even elements of A, followed by all the odd elements of A.
+// Given an array A of non-negative integers, return an array consisting of all the even elements of A, followed by all the odd elements of A.
 
-You may return any answer array that satisfies this condition.
+// You may return any answer array that satisfies this condition.
 
  
 
-Example 1:
+// Example 1:
 
-Input: [3,1,2,4]
-Output: [2,4,3,1]
-The outputs [4,2,3,1], [2,4,1,3], and [4,2,1,3] would also be accepted.
+// Input: [3,1,2,4]
+// Output: [2,4,3,1]
+// The outputs [4,2,3,1], [2,4,1,3], and [4,2,1,3] would also be accepted.
  
 
 class Solution {
