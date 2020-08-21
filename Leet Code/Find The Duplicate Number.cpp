c9ebee3a6ba76015e5becec704ac91496e6e2131
Akash Modak <akashmodak97@gmail.com>
2020-08-22 01:25:29 +0530

    Find The Duplicate Number

---
+++ Leet Code/Find The Duplicate Number.cpp
@@ -0,0 +1,34 @@
+/* Leet Code */
+/* Title - Find The Duplicate Number */
+/* Created By - Akash Modak */
+/* Date - 22/8/2020 */
+
+// Given an array nums containing n + 1 integers where each integer is between 1 and n (inclusive), prove that at least one duplicate number must exist. Assume that there is only one duplicate number, find the duplicate one.
+
+// Example 1:
+
+// Input: [1,3,4,2,2]
+// Output: 2
+// Example 2:
+
+// Input: [3,1,3,4,2]
+// Output: 3
+// Note:
+
+// You must not modify the array (assume the array is read only).
+// You must use only constant, O(1) extra space.
+// Your runtime complexity should be less than O(n2).
+// There is only one duplicate number in the array, but it could be repeated more than once.
+
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        int i;
+        for(i=0;i<nums.size()-1;i++){
+            if(nums[i]==nums[i+1])
+                break;
+        }
+        return nums[i];
+    }
+};
