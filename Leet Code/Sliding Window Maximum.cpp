7f44eb119ab45ecef02a925d080783d9f5142729
Akash Modak <akashmodak97@gmail.com>
2020-09-23 03:13:52 +0530

    Update Sliding Window Maximum.cpp

--- Leet Code/Sliding Window Maximum.cpp
+++ Leet Code/Sliding Window Maximum.cpp
@@ -1,7 +1,7 @@
 /* Leet Code */
 /* Title - Sliding Window Maximum */
 /* Created By - Akash Modak */
-/* Date - 23/09/2020 *
+/* Date - 23/09/2020 */
 
 // You are given an array of integers nums, there is a sliding window of size k which is moving from the very left of the array to the very right. You can only see the k numbers in the window. Each time the sliding window moves right by one position.
 
