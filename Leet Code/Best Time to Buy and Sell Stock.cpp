533d655ff74847a7492ace6e698440de36f45f5d
Akash Modak <akashmodak97@gmail.com>
2020-09-24 21:16:42 +0530

    Best Time to Buy and Sell Stock

---
+++ Leet Code/Best Time to Buy and Sell Stock.cpp
@@ -0,0 +1,43 @@
+/* Leet Code */
+/* Title - Best Time to Buy and Sell Stock */
+/* Created By - Akash Modak */
+/* Date - 24/09/2020 */
+
+// Say you have an array for which the ith element is the price of a given stock on day i.
+
+// If you were only permitted to complete at most one transaction (i.e., buy one and sell one share of the stock), design an algorithm to find the maximum profit.
+
+// Note that you cannot sell a stock before you buy one.
+
+// Example 1:
+
+// Input: [7,1,5,3,6,4]
+// Output: 5
+// Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+//              Not 7-1 = 6, as selling price needs to be larger than buying price.
+// Example 2:
+
+// Input: [7,6,4,3,1]
+// Output: 0
+// Explanation: In this case, no transaction is done, i.e. max profit = 0.
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        if(n<=1)
+            return 0;
+        if(n==2 and prices[1]>prices[0])
+            return prices[1]-prices[0];
+        int minp = INT_MAX;
+        int maxp =0;
+        for(int i=0;i<n;i++){
+            if(prices[i]<minp){
+                minp = prices[i];
+            }
+            else if(prices[i]-minp>maxp)
+                maxp = prices[i]-minp;
+        }
+        return maxp;
+    }
+};
