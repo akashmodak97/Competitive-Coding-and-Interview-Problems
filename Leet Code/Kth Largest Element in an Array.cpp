c8c1fe58822e78e1bb9cdff6be4ea3bec01ebad3
Akash Modak <akashmodak97@gmail.com>
2021-08-10 00:15:20 +0530

    Kth Largest Element in an Array

---
+++ Leet Code/Kth Largest Element in an Array.cpp
@@ -0,0 +1,28 @@
+/* Leet Code */
+/* Title - Kth Largest Element in an Array */
+/* Created By - Akash Modak */
+/* Date - 10/08/2021 */
+
+// Given an integer array nums and an integer k, return the kth largest element in the array.
+
+// Note that it is the kth largest element in the sorted order, not the kth distinct element.
+
+ 
+
+// Example 1:
+
+// Input: nums = [3,2,1,5,6,4], k = 2
+// Output: 5
+// Example 2:
+
+// Input: nums = [3,2,3,1,2,4,5,5,6], k = 4
+// Output: 4
+
+
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        sort(nums.begin(), nums.end(), greater<int>());
+        return nums[k-1];
+    }
+};
