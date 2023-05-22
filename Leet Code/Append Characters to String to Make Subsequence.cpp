6bf901588dddbeb4cf1ce0318e8bc6e0dd5c57a2
Akash Modak <akashmodak97@gmail.com>
2023-05-22 16:35:34 +0530

    Create Append Characters to String to Make Subsequence.cpp

---
+++ Leet Code/Append Characters to String to Make Subsequence.cpp
@@ -0,0 +1,42 @@
+/* Leet Code */
+/* Title - Append Characters to String to Make Subsequence */
+/* Created By - Akash Modak */
+/* Date - 22/05/2023 */
+
+// You are given two strings s and t consisting of only lowercase English letters.
+
+// Return the minimum number of characters that need to be appended to the end of s so that t becomes a subsequence of s.
+
+// A subsequence is a string that can be derived from another string by deleting some or no characters without changing the order of the remaining characters.
+
+ 
+
+// Example 1:
+
+// Input: s = "coaching", t = "coding"
+// Output: 4
+// Explanation: Append the characters "ding" to the end of s so that s = "coachingding".
+// Now, t is a subsequence of s ("coachingding").
+// It can be shown that appending any 3 characters to the end of s will never make t a subsequence.
+// Example 2:
+
+// Input: s = "abcde", t = "a"
+// Output: 0
+// Explanation: t is already a subsequence of s ("abcde").
+// Example 3:
+
+// Input: s = "z", t = "abcde"
+// Output: 5
+// Explanation: Append the characters "abcde" to the end of s so that s = "zabcde".
+// Now, t is a subsequence of s ("zabcde").
+// It can be shown that appending any 4 characters to the end of s will never make t a subsequence.
+
+class Solution {
+public:
+    int appendCharacters(string s, string t) {
+        int j=0;
+        for(int i=0;i<s.length();i++)
+            if(s[i]==t[j])j++;
+        return t.length()-j;
+    }
+};
