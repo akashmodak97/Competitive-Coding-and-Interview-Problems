e38cec9a777e0ecca6afa82bbb5674cc39b08487
Akash Modak <akashmodak97@gmail.com>
2023-05-26 12:48:59 +0530

    Create Maximum Twin Sum of a Linked List.cpp

---
+++ Leet Code/Maximum Twin Sum of a Linked List.cpp
@@ -0,0 +1,73 @@
+/* Leet Code */
+/* Title - Maximum Twin Sum of a Linked List */
+/* Created By - Akash Modak */
+/* Date - 26/05/2023 */
+
+// In a linked list of size n, where n is even, the ith node (0-indexed) of the linked list is known as the twin of the (n-1-i)th node, if 0 <= i <= (n / 2) - 1.
+
+// For example, if n = 4, then node 0 is the twin of node 3, and node 1 is the twin of node 2. These are the only nodes with twins for n = 4.
+// The twin sum is defined as the sum of a node and its twin.
+
+// Given the head of a linked list with even length, return the maximum twin sum of the linked list.
+
+ 
+
+// Example 1:
+
+
+// Input: head = [5,4,2,1]
+// Output: 6
+// Explanation:
+// Nodes 0 and 1 are the twins of nodes 3 and 2, respectively. All have twin sum = 6.
+// There are no other nodes with twins in the linked list.
+// Thus, the maximum twin sum of the linked list is 6. 
+// Example 2:
+
+
+// Input: head = [4,2,2,3]
+// Output: 7
+// Explanation:
+// The nodes with twins present in this linked list are:
+// - Node 0 is the twin of node 3 having a twin sum of 4 + 3 = 7.
+// - Node 1 is the twin of node 2 having a twin sum of 2 + 2 = 4.
+// Thus, the maximum twin sum of the linked list is max(7, 4) = 7. 
+// Example 3:
+
+
+// Input: head = [1,100000]
+// Output: 100001
+// Explanation:
+// There is only one node with a twin in the linked list having twin sum of 1 + 100000 = 100001.
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+class Solution {
+public:
+    int pairSum(ListNode* head) {
+        ListNode* temp1=head, *temp=head;
+        int count=0,i=0,t=0,maxCount=0;
+        while(temp!=NULL)temp=temp->next, count++;
+        vector<int> v(count/2);
+        t=count/2;
+        while(t--){
+            v[i++]=temp1->val;
+            temp1=temp1->next;
+        }
+        t=count/2;
+        while(t--){
+            int x=v[--i]+temp1->val;
+            temp1=temp1->next;
+            maxCount=max(maxCount,x);
+        }
+        return maxCount;
+    }
+};
